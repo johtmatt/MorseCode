ff8f497232a46a59e03adf2b9e4bc9b9020c7b26
johtmatt <31523483+johtmatt@users.noreply.github.com>
2018-02-06 09:04:37 -0600

    Added more code for Part 4
    Tweaked some typos and added more of the code for part 4. NOT WORKING.

--- app/src/main/java/com/example/tmjtb/morsecode/MainActivity.kt
+++ app/src/main/java/com/example/tmjtb/morsecode/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.tmjtb.morsecode
 import kotlinx.android.synthetic.main.content_main.*
 import android.app.Activity
 import android.content.Context
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -15,6 +18,9 @@ import android.view.inputmethod.InputMethodManager
 
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
+import java.lang.Math.round
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
 
@@ -128,11 +134,9 @@ class MainActivity : AppCompatActivity() {
     fun translate(s:String) : String {
 
         var s2 = s.toLowerCase()
-
         var r = ""
 
         for (c in s2) {
-
             if (c == ' ')
                 r += " / "
             else /*if*/ (letToCodeDict.containsKey(c.toString()))
@@ -140,11 +144,10 @@ class MainActivity : AppCompatActivity() {
             /*else
                 r += "?"*/
         }
-
         return r
     }
 
-    fun playString(S:String, i: Int = 0) : Unit {
+    fun playString(s:String, i: Int = 0) : Unit {
 
         if (i > s.length - 1)
             return;
@@ -158,7 +161,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         var c = s[i]
-        Lod.d("Log", "Processing psl: " + i + " char: [" + c + "]")
+        Log.d("Log", "Processing psl: " + i + " char: [" + c + "]")
 
         if (c == '.')
             playDot(thenFun)
@@ -191,6 +194,54 @@ class MainActivity : AppCompatActivity() {
         Timer().schedule(timerTask {
             onDone()
         }, durationMSec.toLong())
+    }
+
+    val SAMPLE_RATE = 44100
+
+    private fun genSineWaveSoundBuffer(frequency: Double, durationMSec: Int) : ShortArray {
+        val duration: Int = round((durationMSec / 1000.0) * SAMPLE_RATE).toInt()
+
+        var mSound : Double
+        val mBuffer = ShortArray(duration)
+        for (i in 0 until duration) {
+            mSound = Math.sin(2.0 * Math.PI * i.toDouble() / (SAMPLE_RATE / frequency))
+            mBuffer[i] = (mSound * java.lang.Short.MAX_VALUE).toShort()
+        }
+        return mBuffer
+    }
+
+    private fun playSoundBuffer(mBuffer:ShortArray, onDone : () -> Unit = {}) {
+        var minBufferSize = SAMPLE_RATE/10
+        if (minBufferSize < mBuffer.size) {
+            minBufferSize = minBufferSize + minBufferSize *
+                    (Math.round(mBuffer.size.toFloat()) / minBufferSize.toFloat()).toInt()
+        }
+
+        val nBuffer = ShortArray(minBufferSize)
+        for (i in nBuffer.indices) {
+            if (i < mBuffer.size)
+                nBuffer[i] = mBuffer[i]
+            else
+                nBuffer[i] = 0
+        }
+
+        val mAudioTrack = AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE,
+        AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
+        minBufferSize, AudioTrack.MODE_STREAM)
+
+        mAudioTrack.setStereoVolume(AudioTrack.getMaxVolume(), AudioTrack.getMaxVolume())
+        mAudioTrack.setNotificationMarkerPosition(mBuffer.size)
+        mAudioTrack.setPlaybackPositionUpdateListener(object : AudioTrack.OnPlaybackPositionUpdateListener {
+            override fun onPeriodNotification(track: AudioTrack) {}
+            override fun onMarkerReached(track: AudioTrack) {
+                Log.d("Log", "Audio track end of file reached...")
+                mAudioTrack.stop()
+                mAudioTrack.release()
+                onDone()
+            }
+        })
+        mAudioTrack.play()
+        mAudioTrack.write(nBuffer, 0, minBufferSize)
     }
 
 }
